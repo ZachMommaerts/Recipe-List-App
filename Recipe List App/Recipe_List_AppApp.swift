ef311347f63d189da2f8224fc88deeaea17d8e4f
ZachMommaerts <zmommaerts@gmail.com>
2023-03-07 11:46:17 -0600

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Zach Mommaerts on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
