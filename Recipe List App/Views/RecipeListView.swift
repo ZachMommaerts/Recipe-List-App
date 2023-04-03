0fdfb73befa4be0d4ee929e962c2f5c7759c6f86
ZachMommaerts <zmommaerts@gmail.com>
2023-04-02 21:59:35 -0500

    - Change SwiftUI list to scrollview, lazyVStack and foreach - Hide navigationbartitle and use text element for title

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -13,21 +13,36 @@ struct RecipeListView: View {
     
     var body: some View {
         NavigationView{
-            List(model.recipes){ recipe in
+            VStack(alignment: .leading) {
                 
-                NavigationLink(destination: RecipeDetailView(recipe:recipe), label: {
-                    HStack(spacing: 20.0){
-                        Image(recipe.image)
-                            .resizable()
-                            .scaledToFill()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(recipe.name)
+                Text("All Recipes")
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                    .bold()
+                
+                
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        ForEach(model.recipes){ recipe in
+                            
+                            NavigationLink(destination: RecipeDetailView(recipe:recipe), label: {
+                                HStack(spacing: 20.0){
+                                    Image(recipe.image)
+                                        .resizable()
+                                        .scaledToFill()
+                                        .frame(width: 50, height: 50, alignment: .center)
+                                        .clipped()
+                                        .cornerRadius(5)
+                                    Text(recipe.name)
+                                        .foregroundColor(Color.black)
+                                }
+                            })
+                        }
+                        .navigationBarHidden(true)
+                        .padding(.leading)
                     }
-                })
+                }
             }
-            .navigationBarTitle("All Recipes")
         }
     }
 }
@@ -35,5 +50,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
